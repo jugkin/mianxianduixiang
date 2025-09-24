96b751fcffcb38afacf0577666c39fcde1d4ff1b
Kevin <3302354063@qq.com>
2025-09-24 19:09:27 +0800

    面向对象 test

---
+++ girlfriend.java
@@ -0,0 +1,11 @@
+public class girlfriend {
+    String name;
+    int age;
+    double weight;
+    public static void cook(){
+        System.out.println("正在做饭");
+    }
+    public static void get(){
+        System.out.println("拿快递");
+    }
+}
