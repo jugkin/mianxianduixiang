8cbb77b94b65cf762bb7dc8ca1c121d2b2501044
Kevin <3302354063@qq.com>
2025-09-24 19:14:07 +0800

    面向对象 test

---
+++ girlfriendtest.java
@@ -0,0 +1,11 @@
+public class girlfriendtest {
+    public static void main(String[] args) {
+       girlfriend g=new girlfriend();
+       g.age=18;
+       g.weight=80.8;
+       System.out.println(g.age);
+       System.out.println(g.weight);
+       girlfriend g1=new girlfriend();
+        System.out.println(g1.age);
+    }
+}
